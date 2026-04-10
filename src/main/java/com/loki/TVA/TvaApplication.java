9d4c66958b9fa2a80c0efe2faf8b851c764b16ce
Vivek Yadav <yadulinux@gmail.com>
2026-04-11 01:10:27 +0530

    initial commit

---
+++ src/main/java/com/loki/TVA/TvaApplication.java
@@ -0,0 +1,13 @@
+package com.loki.TVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvaApplication.class, args);
+	}
+
+}
